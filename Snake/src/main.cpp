26c540c9b4a00f825ae6f4fdf84f9bd401a34286
pyvyx <pyvyx@web.de>
2022-09-12 15:29:41 +0200

    Added push script and seperate header file for clangs WinMain() definition

--- Snake/src/main.cpp
+++ Snake/src/main.cpp
@@ -6,17 +6,7 @@
 
 #include "Snake.h"
 #include "Constants.h"
-
-/*
-    For some reason clang preferably links against main() if available even though the build
-    settings would link against WinMain().
-    However in order to link against WinMain() I created this macro.
-    GCC & MSVC properly link against WinMain().
-    Premake will define all these macros for you if you select clang as compiler.
-*/
-#if defined CLANG && defined RELEASE && defined WINDOWS
-    #define main() WinMain([[maybe_unused]]void* hInst, [[maybe_unused]]void* hInstPrev, [[maybe_unused]]char* cmdline, [[maybe_unused]]int cmdshow)
-#endif
+#include "Clang.h"
 
 
 constexpr std::array<Rectangle, Const::GridSize> GenerateGrid()
