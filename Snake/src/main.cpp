e93f9f460f85fc74c4de1aaa9f9779ebfa41ca31
cunterrainer <vyx3327@web.de>
2024-07-23 17:25:21 +0200

    Added seperate gameloop for wasm

--- Snake/src/main.cpp
+++ Snake/src/main.cpp
@@ -13,6 +13,83 @@
 #include "PauseMenu.h"
 
 
+#ifdef PLATFORM_WEB
+#include <emscripten/emscripten.h>
+
+struct GameObjects
+{
+    Color backgroundColor;
+    Color gridColor;
+
+    Layer* currentLayer;
+    std::vector<std::unique_ptr<Layer>>::iterator layerIt;
+    bool win;
+    Layer* pauseLayer;
+};
+
+
+void GameLoop(void* args)
+{
+    GameObjects* obj = (GameObjects*)args;
+
+    const float deltaTime = GetFrameTime();
+    BeginDrawing();
+    ClearBackground(obj->backgroundColor);
+    for (const Rectangle& cell : Const::Grid)
+        DrawRectangleLinesEx(cell, Const::GridOutlineThickness, obj->gridColor);
+
+    int keyPressed = GetKeyPressed();
+    if (IsGamepadAvailable(0))
+    {
+        if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_FACE_UP))
+            keyPressed = KEY_W;
+        else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_FACE_RIGHT))
+            keyPressed = KEY_D;
+        else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_FACE_DOWN))
+            keyPressed = KEY_S;
+        else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_FACE_LEFT))
+            keyPressed = KEY_A;
+        else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
+            keyPressed = KEY_SPACE;
+        else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_MIDDLE_RIGHT))
+            keyPressed = KEY_ESCAPE;
+        else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_FACE_UP))
+            keyPressed = KEY_R;
+
+    }
+    if (keyPressed == KEY_ESCAPE && obj->currentLayer != obj->pauseLayer)
+    {
+        obj->currentLayer = obj->pauseLayer;
+        keyPressed = KEY_NULL;
+    }
+
+
+    obj->currentLayer->SetWin(obj->win);
+    obj->currentLayer->OnKeyPress(keyPressed, deltaTime);
+    obj->currentLayer->OnUpdate(deltaTime);
+    obj->currentLayer->OnRender();
+    obj->win = obj->currentLayer->Win();
+
+    const LayerStage stage = obj->currentLayer->Done();
+    if (stage == LayerStage::Done)
+    {
+        obj->layerIt = std::next(obj->layerIt);
+        obj->currentLayer = obj->layerIt->get();
+    }
+    else if (stage == LayerStage::DoneEndScreen)
+    {
+        obj->layerIt = std::prev(obj->layerIt);
+        obj->currentLayer = obj->layerIt->get();
+    }
+    else if (stage == LayerStage::DonePause)
+    {
+        obj->currentLayer = obj->layerIt->get();
+    }
+    EndDrawing();
+}
+#endif // PLATFORM_WEB
+
+
 template <class T> constexpr void PushBackLayer(std::vector<std::unique_ptr<Layer>>& layers)
 {
     layers.push_back(std::make_unique<T>());
@@ -38,6 +115,16 @@ int main()
     Layer* currentLayer = layerIt->get();
     bool win = currentLayer->Win();
 
+#ifdef PLATFORM_WEB
+    GameObjects objects;
+    objects.backgroundColor = backgroundColor;
+    objects.gridColor = gridColor;
+    objects.win = win;
+    objects.currentLayer = currentLayer;
+    objects.layerIt = layerIt;
+    objects.pauseLayer = pauseLayer.get();
+    emscripten_set_main_loop_arg(GameLoop, &objects, 0, 1);
+#else
     while (!WindowShouldClose())
     {
         const float deltaTime = GetFrameTime();
@@ -61,7 +148,7 @@ int main()
                 keyPressed = KEY_SPACE;
             else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_MIDDLE_RIGHT))
                 keyPressed = KEY_ESCAPE;
-            else if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_FACE_UP))
+            else if (IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_FACE_UP))
                 keyPressed = KEY_R;
 
         }
@@ -84,7 +171,7 @@ int main()
             layerIt = std::next(layerIt);
             currentLayer = layerIt->get();
         }
-        else if (stage == LayerStage::DoneEndScreen) 
+        else if (stage == LayerStage::DoneEndScreen)
         {
             layerIt = std::prev(layerIt);
             currentLayer = layerIt->get();
@@ -95,5 +182,6 @@ int main()
         }
         EndDrawing();
     }
+#endif
     TerminateWindow();
 }
